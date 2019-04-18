6b0721169ff9cea29566bdc6922d6e2f93446c1e
Long Le <lle26@kings.kucits.ca>
2019-04-18 15:36:10 -0400

    update validatetoken function and implemented test cases

--- src/main.pl
+++ src/main.pl
@@ -14,10 +14,10 @@ require "../lib/jwt-0.024.lib";
 sub Main() {
 
     ### SetToken function testing ###
-    print STDOUT "Test cases for the function SetToken()...\n\n";
-    # Successful creation of JWT
-    my $jwt = &JWT::SetToken({ name => "Long Le", user => "admin" }, "nososecret");
-    print STDOUT "Test 0: Data returned from success\n$jwt\n\n";
+    print STDOUT "SetToken() function test cases...\n\n";
+    # JWT returned from successful encoding
+    my $jwt = &JWT::SetToken({ name => "Long Le", user => "admin" }, "notsosecret");
+    print STDOUT "Test 1: JWT returned from successful encoding\n$jwt \n\n";
 
     # No input parameters
     my $jwt1 = &JWT::SetToken();
@@ -27,9 +27,9 @@ sub Main() {
     }
     print STDOUT "\n";
 
-    # Empty first input parameter
+    # Single empty parameter
     my $jwt2 = &JWT::SetToken("");
-    print STDOUT "Test 3: Empty first input parameter\n";
+    print STDOUT "Test 3: Single empty parameter\n";
     foreach my $key (sort keys %{ $jwt2 }) {
         print STDOUT "$key: $jwt2->{$key}\n";
     }
@@ -43,13 +43,65 @@ sub Main() {
     }
     print STDOUT "\n";
 
-    # Token and empty key
+    # Valid token and empty key
     my $jwt4 = &JWT::SetToken({ name => "Long Le", user => "admin" }, "");
     print STDOUT "Test 5: Valid token and empty key\n";
     foreach my $key (sort keys %{ $jwt4 }) {
         print STDOUT "$key: $jwt4->{$key}\n";
     }
+    print STDOUT "\n\n";
+    ### SetToken function testing END ###
+
+    ### ValidateToken function testing ###
+    # Valid token and key
+    print STDOUT "ValidateToken() function test cases...\n\n";
+    my $jwt_validate = &JWT::ValidateToken($jwt, "notsosecret");
+    print STDOUT "Test 6: Valid token and key\n";
+    foreach my $key (sort keys %{ $jwt_validate }) {
+        print STDOUT "$key: $jwt_validate->{$key}\n";
+    }
     print STDOUT "\n";
+
+    # Valid token and invalid key
+    my $jwt_validate1 = &JWT::ValidateToken($jwt, "sosecret");
+    print STDOUT "Test 7: Valid token and invalid key\n";
+    foreach my $key (sort keys %{ $jwt_validate1 }) {
+        print STDOUT "$key: $jwt_validate1->{$key}\n";
+    }
+    print STDOUT "\n";
+
+    # Valid token and empty key
+    my $jwt_validate2 = &JWT::ValidateToken($jwt, "");
+    print STDOUT "Test 8: Valid token and empty key\n";
+    foreach my $key (sort keys %{ $jwt_validate2 }) {
+        print STDOUT "$key: $jwt_validate2->{$key}\n";
+    }
+    print STDOUT "\n";
+
+    # Empty token and empty key
+    my $jwt_validate3 = &JWT::ValidateToken("", "");
+    print STDOUT "Test 9: Empty token and empty key\n";
+    foreach my $key (sort keys %{ $jwt_validate3 }) {
+        print STDOUT "$key: $jwt_validate3->{$key}\n";
+    }
+    print STDOUT "\n";
+
+    # No input parameter
+    my $jwt_validate4 = &JWT::ValidateToken();
+    print STDOUT "Test 10: No input parameter\n";
+    foreach my $key (sort keys %{ $jwt_validate4 }) {
+        print STDOUT "$key: $jwt_validate4->{$key}\n";
+    }
+    print STDOUT "\n";
+
+    # One empty parameter
+    my $jwt_validate5 = &JWT::ValidateToken("");
+    print STDOUT "Test 11: One empty parameter\n";
+    foreach my $key (sort keys %{ $jwt_validate5 }) {
+        print STDOUT "$key: $jwt_validate5->{$key}\n";
+    }
+    print STDOUT "\n\n";
+    ### ValidateToken function testing END###
 
 };
 
